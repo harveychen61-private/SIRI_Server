cbdb2ef59ba237b21d90a77c1d7da0c70b830752
Harvey Chen <harvey.chen@transport.nsw.gov.au>
2019-07-10 16:37:20 +1000

    Manage Subscription Request

--- Controllers/SiriManager.cs
+++ Controllers/SiriManager.cs
@@ -1,11 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using SIRI.Models;
 
 namespace SIRI.Controllers
 {
-    public class SiriServiceInformation{
-        public DateTime ServiceStartTime {get;set;}
+    public class SiriServiceInformation
+    {
+        public DateTime ServiceStartTime { get; set; }
     }
+
+
+
+
 
 
     public class SiriManager
@@ -49,6 +56,7 @@ namespace SIRI.Controllers
 
                 case "SIRI.Models.SubscriptionRequest":
                     System.Console.WriteLine("SubscriptionRequest");
+                    createSubscriptionResponse();
                     status = Status.ValidAndResponse;
                     break;
 
@@ -69,7 +77,7 @@ namespace SIRI.Controllers
                     break;
 
                 case "SIRI.Models.SubscriptionResponseStructure":
-                    System.Console.WriteLine("Heartbeat");
+                    System.Console.WriteLine("SubscriptionResponse");
                     status = Status.ValidNoResponse;
                     break;
 
@@ -103,22 +111,109 @@ namespace SIRI.Controllers
         // create check status response
         private void createCheckStatusResponse()
         {
-             Siri responseSiri = new Siri();
-             CheckStatusRequestStructure checkStatusRequest = (CheckStatusRequestStructure)siri.Item;
-             
-             CheckStatusResponseStructure csr = new CheckStatusResponseStructure();
-             csr.ResponseTimestamp = DateTime.Now;
-             csr.Status=true;
-             csr.ServiceStartedTime = serviceInfo.ServiceStartTime;
-             if (checkStatusRequest.MessageIdentifier!=null){
-                 MessageRefStructure requestMessageRef = new MessageRefStructure();
-                 requestMessageRef.Value=checkStatusRequest.MessageIdentifier.Value;
-                 csr.RequestMessageRef = requestMessageRef;
-             }
-             responseSiri.Item=csr;
-             this.response=responseSiri;
+            Siri responseSiri = new Siri();
+            CheckStatusRequestStructure checkStatusRequest = (CheckStatusRequestStructure)siri.Item;
+
+            CheckStatusResponseStructure csr = new CheckStatusResponseStructure();
+            csr.ResponseTimestamp = DateTime.Now;
+            csr.Status = true;
+            csr.ServiceStartedTime = serviceInfo.ServiceStartTime;
+            if (checkStatusRequest.MessageIdentifier != null)
+            {
+                MessageRefStructure requestMessageRef = new MessageRefStructure();
+                requestMessageRef.Value = checkStatusRequest.MessageIdentifier.Value;
+                csr.RequestMessageRef = requestMessageRef;
+            }
+            responseSiri.Item = csr;
+            this.response = responseSiri;
 
         }//createCheckStatusResponse
+
+
+
+        // create subsription response
+        private void createSubscriptionResponse()
+        {
+            Siri responseSiri = new Siri();
+            SubscriptionResponseStructure srs = new SubscriptionResponseStructure();
+            srs.ResponseTimestamp = DateTime.Now;
+            ParticipantRefStructure responder = new ParticipantRefStructure();
+            responder.Value = "TfNSW_SIRI_Service";
+            srs.ResponderRef = responder;
+            List<StatusResponseStructure> responseStatus = new List<StatusResponseStructure>();
+
+
+
+            //identify the subscription information
+            SubscriptionRequest sr = (SubscriptionRequest)siri.Item;
+            //esitmated timetable susbcription request
+            if (sr.EstimatedTimetableSubscriptionRequest != null)
+            {
+
+                for (int i = 0; i < sr.EstimatedTimetableSubscriptionRequest.Length; i++)
+                {
+                    responseStatus.Add(createSubscriptionResponseStatus(sr.EstimatedTimetableSubscriptionRequest[i]));
+                }
+
+            }
+
+            //situation exchange subscription request
+            if (sr.SituationExchangeSubscriptionRequest != null)
+            {
+
+                for (int i = 0; i < sr.SituationExchangeSubscriptionRequest.Length; i++)
+                {
+                    responseStatus.Add(createSubscriptionResponseStatus(sr.SituationExchangeSubscriptionRequest[i]));
+                }
+
+            }
+
+            //vehicle monitoring subscription request
+            if (sr.VehicleMonitoringSubscriptionRequest != null)
+            {
+
+                for (int i = 0; i < sr.VehicleMonitoringSubscriptionRequest.Length; i++)
+                {
+                    responseStatus.Add(createSubscriptionResponseStatus(sr.VehicleMonitoringSubscriptionRequest[i]));
+                }
+
+            }
+
+
+            srs.ResponseStatus = responseStatus.ToArray();
+            responseSiri.Item = srs;
+            this.response = responseSiri;
+
+        }//createSubscriptionResponse
+
+
+        public StatusResponseStructure createSubscriptionResponseStatus(AbstractSubscriptionStructure inputSubscription)
+        {
+            int maxExistingId = 0;
+            if (Repository.Subscriptions.Count > 0)
+            {
+                maxExistingId = Repository.Subscriptions.Keys.Max();
+
+            }
+
+            SiriSubscription siriSubscription = new SiriSubscription();
+            siriSubscription.subscription = (AbstractSubscriptionStructure)inputSubscription;
+
+            StatusResponseStructure statusResponse = new StatusResponseStructure();
+            statusResponse.ResponseTimestamp = DateTime.Now;
+            statusResponse.SubscriberRef = inputSubscription.SubscriberRef;
+            statusResponse.SubscriptionRef = inputSubscription.SubscriptionIdentifier;
+            statusResponse.Status = true;
+            statusResponse.ShortestPossibleCycle = null;
+            siriSubscription.subscriptionResponse = statusResponse;
+            Repository.Subscriptions.Add(maxExistingId + 1, siriSubscription);
+            return statusResponse;
+
+        }//createSubscriptionResponseStatus
+
+
+
+
 
 
     }//siriManager
--- Models/Repository.cs
+++ Models/Repository.cs
@@ -2,14 +2,23 @@ using System.Collections.Generic;
 
 namespace SIRI.Models
 {
+    public struct SiriSubscription{
+        public AbstractSubscriptionStructure subscription;
+        public StatusResponseStructure subscriptionResponse;
+
+    } 
+
 
     public class Repository
     {
         public static IDictionary<int, Siri> Siris { get; set; }
 
+        public static IDictionary<int, SiriSubscription> Subscriptions { get; set; }
+
         static Repository()
         {
             Siris = new Dictionary<int, Siri>();
+            Subscriptions = new Dictionary<int, SiriSubscription>();
 
         }
 
